d0e2e4bbd330fca378c3485f69f0f62d4d0babb1
kerneq <stasik-0@yandex.ru>
2017-01-27 23:12:14 +0300

    fix callback page and add 404 page

--- mr/settings.py
+++ mr/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -29,8 +28,6 @@ ALLOWED_HOSTS = []
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
-
-print(MEDIA_ROOT)
 
 # Application definition
 
@@ -75,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mr.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -85,7 +81,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -105,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -118,6 +112,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# DEBUG = False
+# TEMPLATE_DEBUG = DEBUG
+# ALLOWED_HOSTS = [
+#    "*"
+# ]
 
 
 # Static files (CSS, JavaScript, Images)
